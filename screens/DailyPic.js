f7342fd23674c97da4643e3cc980094007c7d428
Neel-Jogani <73420273+Neel-Jogani@users.noreply.github.com>
2021-08-07 11:03:16 +0530

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import{View, Text, TouchbleOpacity, StyleSheet} from 'react-native'
+
+export default class DailyPic extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Pictures</Text>
+            </View>
+        )
+    }
+}
---
+++ screens/SpaceCrafts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import{View, Text, TouchbleOpacity, StyleSheet} from 'react-native'
+
+export default class SpaceCrafts extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Space Crafts</Text>
+            </View>
+        )
+    }
+}
